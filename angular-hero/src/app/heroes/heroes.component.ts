5d36eb5c618cfe622348d4457f9a5bce01c7d547
Sam <huangsaiy@hotmail.com>
2018-02-17 23:52:50 +1100

    display panel changed.

--- angular-hero/src/app/heroes/heroes.component.ts
+++ angular-hero/src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+ 
 import { Hero } from '../hero';
-import {HEROES } from '../mock-heroes';
 import { HeroService } from '../service/hero.service';
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,16 +10,15 @@ import { HeroService } from '../service/hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+ 
   constructor(private heroService: HeroService) { }
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+ 
   ngOnInit() {
     this.getHeroes();
   }
-
+ 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
