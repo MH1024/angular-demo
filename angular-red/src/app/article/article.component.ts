8a3867a39b5a41fee027ba509f8dcadf20acf1b0
Sam <huangsaiy@hotmail.com>
2018-04-24 00:02:07 +1000

    use article module.

--- angular-red/src/app/article/article.component.ts
+++ angular-red/src/app/article/article.component.ts
@@ -1,28 +1,23 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-
+import { Article } from './article.module';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
- @HostBinding('attr.class') cssClass = 'row';
- votes: number;
- title: string;
- link: string;
-  constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
-   }
-
-   voteUp() {
-     this.votes += 1;
-   }
-   voteDown() {
-     this.votes -= 1;
-   }
+export class ArticleComponent implements OnInit { @HostBinding('attr.class') cssClass = 'row'; article: Article;
+constructor() {
+this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+}
+voteUp(): boolean {
+  this.article.votes += 1; return false;
+}
+voteDown(): boolean {
+  this.article.votes -= 1; return false;
+}
   ngOnInit() {
   }
-
 }
--- angular-red/src/app/article/article.module.ts
+++ angular-red/src/app/article/article.module.ts
@@ -2,7 +2,8 @@ export class Article {
     title: string;
     link: string;
     votes: number;
-    constructor(title: string, link: string, votes?: number) { this.title = title;
+    constructor(title: string, link: string, votes?: number) {
+    this.title = title;
     this.link = link;
     this.votes = votes || 0;
     }
