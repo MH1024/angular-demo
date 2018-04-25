2f4fe9397496e334403361a34e0aa81c1201a1bf
Sam <huangsaiy@hotmail.com>
2018-04-26 00:00:52 +1000

    [article]add sort article func.

--- angular-red/src/app/app.component.ts
+++ angular-red/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { log } from 'util';
+import { Article } from './article/article.module';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,12 @@ import { log } from 'util';
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+  sortedArticles(): Article[] {
+    return this.sortedArticles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
